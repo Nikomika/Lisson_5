366e14fc48518b843d05d2fd729695599ba95ea4
Nikomika <o.a.voytova@gmail.com>
2025-01-14 22:05:24 +0300

    New message commit

--- src/MyArraySizeException.java
+++ src/MyArraySizeException.java
@@ -1,5 +1,5 @@
 public class MyArraySizeException extends RuntimeException {
     public MyArraySizeException() {
-            super("Размер массива не корректный!");;
+            super("Размер массива не подходит! Массив должен быть 4х4");;
     }
 }
